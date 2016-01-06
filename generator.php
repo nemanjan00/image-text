f9f68ae952e6c45d5fa4f2a87d0970e52fba58b1
Nemanja Nedeljković <nemanjan00@gmail.com>
2016-01-07 00:36:14 +0100

    Refactoring

--- generator.php
+++ generator.php
@@ -1,10 +1,6 @@
 <?php
 require_once __DIR__."/vendor/autoload.php";
 
-class ImageGenerator {
-
-}
-
 class Arguments {
 	private $arguments;
 
--- plugins/arguments.cli.commando.php
+++ plugins/arguments.cli.commando.php
@@ -1,24 +1,4 @@
 <?php
-$plugins->registerPlugin("arguments.cli", "ArgumentsCli");
-
-class ArgumentsCli {
-	private $arguments;
-
-	private $commando;
-
-	public function __construct(&$arguments, $plugins){
-		$this->arguments = $arguments;
-
-		$commando = new Commando\Command();		
-
-		$plugins->executeWorkspace("arguments.cli.commando", $commando);
-
-		foreach($commando->getOptions() as $key => $value){
-			$arguments->setArgument($key, $value->getValue());
-		}
-	}	
-}
-
 $plugins->registerPlugin("arguments.cli.commando", "ArgumentsCliCommando");
 
 class ArgumentsCliCommando {
